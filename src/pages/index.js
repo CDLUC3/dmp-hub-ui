3ce5493e7919dfdf92ec2b96c29993cb40255f24
Andre Engelbrecht <andre@teh-node.co.za>
2023-05-15 15:35:42 +0100

    WIP: Boilerplate components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,23 @@
+import { useRouter } from 'next/router';
 
 
 function Dashboard() {
+  const router = useRouter();
+
   return (
     <>
-      <div id="Dashboard">Dashboard page</div>
+      <div id="Dashboard">
+        <h2>
+          Dashboard
+          <button onClick={() => router.push('/plan/add')}>
+            Add Plan
+          </button>
+        </h2>
+        <div className="todo">
+          <p>Here we want a responsive table component</p>
+        </div>
+
+      </div>
     </>
   )
 }
